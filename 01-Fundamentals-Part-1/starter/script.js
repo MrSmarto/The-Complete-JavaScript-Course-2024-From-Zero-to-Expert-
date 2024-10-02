9ee14680332207647e2a4185d5f8eb75ba3c3959
Thijmen Smart <thijmen.smart@creativeclicks.com>
2024-10-02 14:05:07 +0200

    yes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Thijmen");
+console.log("23");
+
+// een variabele is een box waar je voorwerpen in stopt. 
+let firstName = "Klara";
+
+console.log("firstName");
+console.log("firstName");
+console.log("firstName");
+
+// functies mogen alleen cijfers, letters, onderstrepingstekens of dollertekens bevatten. De rest is illegaal
+let thijmen_klara = "TK";
+
+// some function names are reserved like: function & new
+let $function = "27";
+
+// use lowercase letters
+let person = "thijmen"
+
+// constante functies hebben een andere kleur en worden herkend. Ook zijn die in caps.
+let PI = 3.1415;
+
+// geef variabelen altijd een logische naam.
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer"; 
+let job2 = "teacher";
